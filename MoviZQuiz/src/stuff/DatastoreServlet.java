3aab8c61803f88a7f3ec6525d6035d8a89dd356f
sbelassa <salma.belassal@gmail.com>
2016-12-14 22:06:46 +0100

    UI integration

--- MoviZQuiz/src/stuff/DatastoreServlet.java
+++ MoviZQuiz/src/stuff/DatastoreServlet.java
@@ -1,12 +1,7 @@
 package stuff;
 
-
 import java.io.IOException;
-
-
-
 import javax.servlet.http.*;
-
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
