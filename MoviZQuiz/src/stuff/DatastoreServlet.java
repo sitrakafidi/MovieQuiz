fe63afb9b39bd6320b010d7ebb1e1a53545e26d1
sitraka <sitraka@sitraka-pc>
2016-12-17 16:19:50 +0100

    create movies in datastore

--- MoviZQuiz/src/stuff/DatastoreServlet.java
+++ MoviZQuiz/src/stuff/DatastoreServlet.java
@@ -9,21 +9,33 @@ import com.google.appengine.api.datastore.Entity;
 @SuppressWarnings("serial")
 public class DatastoreServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)throws IOException {
-		DbpediaQuery dbq = new DbpediaQuery();
-		dbq.queryDbpedia();
+		//DbpediaQuery dbq = new DbpediaQuery();
+		//dbq.queryDbpedia();
 		resp.getWriter().println("creating movies");
 
 
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		int max=10;
-		for (int i = 0; i < max; i++) {
-			Entity e = new Entity("Movie", "m" + i);
+		//int max=10;
+		//for (int i = 0; i < max; i++) {
+			/*Entity e = new Entity("Movie", "m" + i);
 			e.setProperty("title", dbq.getTitles().get(i));
 			e.setProperty("director", dbq.getDirectors().get(i));
-			e.setProperty("year", dbq.getYears().get(i));
+			e.setProperty("year", dbq.getYears().get(i));*/
+			
+			Entity e = new Entity("Movie","m1");
+			e.setProperty("title", "1994");
+			e.setProperty("director", "Elmo Nüganen");
+			e.setProperty("year", "2015");
 			
 			datastore.put(e);
-		}
-		resp.getWriter().println("done");
+			
+			Entity e2 = new Entity("Movie","m2");
+			e2.setProperty("title", "Cornouaille");
+			e2.setProperty("director", "Anne Le Ny");
+			e2.setProperty("year", "2012");
+			
+			datastore.put(e2);
+		//}
+		resp.getWriter().println("done!!!!!!!");
 	}
 }
